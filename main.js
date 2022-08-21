390e5e5a21294c3f5d9b1c94050bdcafa2cd2563
Abhishek1345 <jawanpuriaabhishek@gmail.com>
2022-08-21 12:13:02 +0530

    Sticky-notes v 1.0

--- main.js
+++ main.js
@@ -1,53 +1,95 @@
-window.onload=function(){
-  if(localStorage.getItem("notes")!=null){
-document.querySelector(".content").innerHTML=localStorage.getItem("notes");
-  var l=document.getElementById("val");
-  l.value=localStorage.getItem("notes");
-document.getElementById("limit").innerHTML=100-l.value.length;
-  }
-  else{
-    document.querySelector(".content").innerHTML="Double click to edit";
+var target;
+window.onload = function() {
+  var n=localStorage.getItem("n");
+  makeNotes();
+  if(n!=null){
+    for(var i=0;i<n;i++){
+      makeNotes();
     }
+  }
+  if (localStorage.getItem("notes") != null) {
+    var str=localStorage.getItem("notes");
+    var notes=document.querySelectorAll(".content");
+    for(var i=0;i<notes.length;i++){
+    var subStr=str.slice(0,str.indexOf('$'));
+    notes[i].innerHTML=subStr;
+    str=str.slice(str.indexOf('$')+1);
+    }
+    // l.value = localStorage.getItem("notes");
+    // document.getElementById("limit").innerHTML = 100 - l.value.length;
+  } 
+  else {
+    document.querySelector(".content").innerHTML = "Double click to edit";
   }
   
-  function getWriter(){
-    document.querySelector(".writer").style.display="flex";
-    }
+  }
 
-var note=document.querySelector(".sticky-note");
-note.addEventListener("dblclick",getWriter);
-   
-   function save(){
-     var val=document.getElementById("val").value;
-     var writer=document.querySelector(".writer");
-     var note=document.querySelector(".content");
-     
-     note.innerHTML=val;
-     writer.style.display="none";
-     localStorage.setItem("notes",val);
-     }
+
+function getWriter() {
+  document.querySelector(".writer").style.display = "flex";
   
- document.getElementById("val").addEventListener("keyup",()=>{
- var l=document.getElementById("val").value.length;
-  var limit=document.getElementById("limit");
-  limit.innerHTML=100-l;
-  if(l>=100){
-    limit.style.color="red";
-    }
-    else{
-      limit.style.color="green";
-      }
-  });
-  
-  document.getElementsByClassName("add")[0].addEventListener("click",()=>{
+  var l = document.getElementById("val");
+  l.value=target.getElementsByClassName("content")[0].innerHTML;
+  document.getElementById("limit").innerHTML = 100 - l.value.length;
+}
+
+
+// var note = document.querySelector(".sticky-note");
+
+//   note.addEventListener("dblclick", () => {
+//     target =this;
+//     getWriter();
     
-  var sticky=document.createElement("div");
-  var content=document.createElement("div");
+//   });
+
+
+
+function save() {
+  var val = document.getElementById("val").value;
+  var writer = document.querySelector(".writer");
+  var note = document.querySelectorAll(".content");
+   
+  target.getElementsByClassName("content")[0].innerHTML = val;
+  writer.style.display = "none";
+val="";
+  for(var i=0;i<note.length;i++){
+       val+=note[i].innerHTML+'$';
+  }
   
-  sticky.addEventListener("dblclick",getWriter);
-  sticky.className="sticky-note";
-  content.className="content";
-  sticky.appendChild(content);
-  //document.getElementsByClassName("container")[0].insertBefore(sticky,document.getElementsByClassName("writer")[0]);
-  document.body.insertBefore(sticky,document.getElementsByClassName("writer")[0])
+  localStorage.setItem("notes", val);
+}
+
+function StickyNote(obj, content) {
+  this.obj = obj;
+  this.obj.appendChild(content);
+  document.body.insertBefore(this.obj, document.getElementsByClassName("writer")[0]);
+  this.obj.addEventListener("dblclick", () => {
+    target = obj;
+    getWriter();
+    
+ 
+
   });
+}
+
+document.getElementById("val").addEventListener("keyup", () => {
+  var l = document.getElementById("val").value.length;
+  var limit = document.getElementById("limit");
+  limit.innerHTML = 100 - l;
+  if (l >= 100) {
+    limit.style.color = "red";
+  } else {
+    limit.style.color = "green";
+  }
+});
+
+document.getElementsByClassName("add")[0].addEventListener("click",makeNotes);
+function makeNotes(){
+  var sticky = document.createElement("div");
+  var content = document.createElement("div");
+  var n=document.querySelectorAll(".content").length;
+  sticky.className = "sticky-note";
+  content.className = "content";
+  localStorage.setItem("n",n.toString())
+  var sn = new StickyNote(sticky, content);
+}
